b5edc0388dcdd05d3f23a93d8aefee9c9edf7d39
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-09-19 21:54:02 -0300

    commit 3

--- escola2/escola/views.py
+++ escola2/escola/views.py
@@ -9,6 +9,9 @@ from escola.serializer import (
 )
 from rest_framework import viewsets, generics, filters
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.throttling import UserRateThrottle
+from escola2.escola.throttles import MatriculaAnonRateThrottle
+
 # from rest_framework.authentication import BasicAuthentication
 # from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
 
@@ -16,37 +19,39 @@ from django_filters.rest_framework import DjangoFilterBackend
 class EstudanteViewSet(viewsets.ModelViewSet):
     # authentication_classes = [BasicAuthentication]
     # permission_classes = [IsAuthenticated]
-    queryset = Estudante.objects.all()
+    queryset = Estudante.objects.all().order_by("id")
     # serializer_class = EstudanteSerializer
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     ordering_fields = ["nome"]
     search_fields = ["nome", "cpf"]
-    
+
     def get_serializer_class(self):
         if self.request.version == "v2":
             return EstudanteSerializerV2
         return EstudanteSerializer
 
-
 class CursoViewSet(viewsets.ModelViewSet):
     # authentication_classes = [IsAuthenticated]
     # permission_classes = [IsAuthenticated]
-    queryset = Curso.objects.all()
+    queryset = Curso.objects.all().order_by("id")
     serializer_class = CursoSerializer
 
 
+
+    
 class MatriculaViewSet(viewsets.ModelViewSet):
     # authentication_classes = [BasicAuthentication]
     # permission_classes = [IsAuthenticated]
-    queryset = Matricula.objects.all()
+    queryset = Matricula.objects.all().order_by("id")
     serializer_class = MatriculaSerializer
+    throttle_classes = [UserRateThrottle, MatriculaAnonRateThrottle]
 
 
 class ListaMatriculaEstudante(generics.ListAPIView):
     # authentication_classes = [BasicAuthentication]
     # permission_classes = [IsAuthenticated]
     def get_queryset(self):
-        queryset = Matricula.objects.filter(estudantes_id=self.kwargs["pk"])
+        queryset = Matricula.objects.filter(estudantes_id=self.kwargs["pk"]).order_by("id")
         return queryset
 
     serializer_class = ListaMatriculasEstudantesSerializer
@@ -56,7 +61,7 @@ class ListaMatriculaCurso(generics.ListAPIView):
     # authentication_classes = [BasicAuthentication]
     # permission_classes = [IsAuthenticated]
     def get_queryset(self):
-        queryset = Matricula.objects.filter(curso_id=self.kwargs["pk"])
+        queryset = Matricula.objects.filter(curso_id=self.kwargs["pk"]).order_by("id")
         return queryset
 
     serializer_class = ListaMatriculasCursoSerializer
--- escola2/setup/settings.py
+++ escola2/setup/settings.py
@@ -126,13 +126,21 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.BasicAuthentication",
-    ],
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
-    ],
+    # "DEFAULT_AUTHENTICATION_CLASSES": [
+    #     "rest_framework.authentication.BasicAuthentication",
+    # ],
+    # "DEFAULT_PERMISSION_CLASSES": [
+    #     "rest_framework.permissions.IsAuthenticated",
+    # ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
     "DEFAULT_VERSIONING_CLASS": "rest_framework.versioning.QueryParameterVersioning",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "20/day",
+        "user": "50/day",
+    }
 }
